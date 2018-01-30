bbafb61949ac5f8019324152bd62eec7d96b8888
יהודית אילסון <JudithI@kkl.org.il>
2018-01-30 20:55:27 +0200

    save..

--- client/fuController.js
+++ client/fuController.js
@@ -1,7 +1,32 @@
-fuApp.controller('fuController', function($scope) {
+fuApp.controller('fuController', function($scope, $upload) {
     $scope.upload = function()  {
         var tat = $scope.fily;
         alert('you clicked upload');
-    }    
+    }  
+    
 
+    $scope.formObj = {
+        name: "Test"
+    };
+      
+      var fileToUpload;
+      
+      $scope.onFileSelect = function (file) {
+       fileToUpload = file[0];
+      };
+      
+      // POSt request to /api/items
+      $scope.addItem = function() {
+        console.log($scope.formObj);
+        $scope.upload = $upload.upload({
+         url: '/api/items',
+         method: 'POST',
+         data: { myObj: $scope.formObj },
+         file: fileToUpload
+        }).success(function(data, status, headers, config) {
+         console.log("success");
+        });
+      };
 });
+
+
